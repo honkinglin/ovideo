7953b60e60a5589e05420bb44c2546d39657c710
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-07 08:49:10 +0000

    Deploy HQ-Lin/ovideo to github.com/HQ-Lin/ovideo.git:gh-pages

---
+++ precache-manifest.7f2e6a2443e59d0b553bbf35654ef2fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836efd3278c7ab9ac73c29e2b2ceb04e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518046148f9ded8481ed",
+    "url": "/static/css/main.1c3723cc.chunk.css"
+  },
+  {
+    "revision": "075c1f6ab61a904691df",
+    "url": "/static/js/2.d65f8128.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d65f8128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518046148f9ded8481ed",
+    "url": "/static/js/main.252dcc82.chunk.js"
+  },
+  {
+    "revision": "735bf9b7a49def2419dd",
+    "url": "/static/js/runtime-main.8e994b71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
